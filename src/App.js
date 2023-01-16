29ae03df0a4fd91442dba7172a835e4070b7a2c3
fepanbr <fepanbr@gmail.com>
2023-01-16 15:06:51 +0900

    feat(adder): develop add a todo

--- src/App.js
+++ src/App.js
@@ -4,41 +4,45 @@ import Adder from './components/Adder';
 import Content from './components/Content';
 import Header from './components/Header';
 
-const todos = [
-  {
-    id: 1,
-    content: '공부하기',
-    isCompleted: false,
-  },
-  {
-    id: 2,
-    content: '숙제하기',
-    isCompleted: false,
-  },
-  {
-    id: 3,
-    content: '밥먹기',
-    isCompleted: true,
-  },
-  {
-    id: 4,
-    content: '운동하기',
-    isCompleted: true,
-  },
-];
-
 function App() {
   const [filter, setFilter] = useState('All');
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      content: '공부하기',
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      content: '숙제하기',
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      content: '밥먹기',
+      isCompleted: true,
+    },
+    {
+      id: 4,
+      content: '운동하기',
+      isCompleted: true,
+    },
+  ]);
 
   const handleChangeFilter = (e) => {
     const { value } = e.target;
     setFilter(value);
   };
+
+  const handleAddTodo = (content) => {
+    const lastTodoId = todos.sort((a, b) => a.id - b.id)[todos.length - 1]?.id;
+    setTodos([...todos, { id: lastTodoId + 1, content, isCompleted: false }]);
+  };
   return (
     <div className={styled.app}>
       <Header filter={filter} onClick={handleChangeFilter} />
       <Content filter={filter} todos={todos} />
-      <Adder />
+      <Adder addTodo={handleAddTodo} />
     </div>
   );
 }
--- src/components/Adder.jsx
+++ src/components/Adder.jsx
@@ -1,13 +1,34 @@
-import React from "react";
-import styled from "./Adder.module.css";
+import React, { useState } from 'react';
+import styled from './Adder.module.css';
 
-export default function Adder() {
+export default function Adder({ addTodo }) {
+  const [value, setValue] = useState('');
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setValue(value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!value) return;
+
+    addTodo(value);
+    setValue('');
+  };
+
   return (
     <section className={styled.adder}>
-      <div className={styled.container}>
-        <input className={styled.input} type="text" />
-        <button className={styled.button}>Add</button>
-      </div>
+      <form onSubmit={handleSubmit} className={styled.container}>
+        <input
+          value={value}
+          onChange={handleChange}
+          className={styled.input}
+          type="text"
+        />
+        <button type="submit" className={styled.button}>
+          Add
+        </button>
+      </form>
     </section>
   );
 }
