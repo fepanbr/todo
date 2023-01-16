8e299da1ed100c6bf6891eca34b91cea57f7cf41
fepanbr <fepanbr@gmail.com>
2023-01-16 12:02:13 +0900

    feat(content): show filtered todos

--- src/App.js
+++ src/App.js
@@ -3,11 +3,34 @@ import Adder from "./components/Adder";
 import Content from "./components/Content";
 import Header from "./components/Header";
 
+const todos = [
+  {
+    id: 1,
+    content: "공부하기",
+    isCompleted: false,
+  },
+  {
+    id: 2,
+    content: "숙제하기",
+    isCompleted: false,
+  },
+  {
+    id: 3,
+    content: "밥먹기",
+    isCompleted: true,
+  },
+  {
+    id: 4,
+    content: "운동하기",
+    isCompleted: true,
+  },
+];
+
 function App() {
   return (
     <div className={styled.app}>
       <Header />
-      <Content />
+      <Content filter="completed" todos={todos} />
       <Adder />
     </div>
   );
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -2,11 +2,20 @@ import React from "react";
 import Todo from "./Todo";
 import styled from "./Content.module.css";
 
-export default function Content() {
+export default function Content({ filter, todos }) {
+  if (filter === "all") {
+    return <ul className={styled.content}>{todos.map(generateTodo)}</ul>;
+  }
+
   return (
     <ul className={styled.content}>
-      <Todo />
-      <Todo />
+      {filter === "active"
+        ? todos.filter(({ isCompleted }) => !isCompleted).map(generateTodo)
+        : todos.filter(({ isCompleted }) => isCompleted).map(generateTodo)}
     </ul>
   );
 }
+
+const generateTodo = ({ id, content, isCompleted }) => (
+  <Todo content={content} key={id.toString()} isCompleted={isCompleted} />
+);
